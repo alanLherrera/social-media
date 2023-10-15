fe4ad464a8423cd57ace225100f647207fbe9f27
alanLherrera <alanherrera06@gmail.com>
2023-10-15 16:56:17 -0700

    thread created

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,20 @@
+import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  connectToDB();
+
+  const createThread = await Thread.create();
+}
