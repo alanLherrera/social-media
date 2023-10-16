0ab3a43f8ea2bf5938e6eff51fe2653f15ab83f6
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2023-10-16 13:29:32 -0700

    updated home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+
 export default async function Home() {
-  const result = await fetchPosts();
+  const result = await fetchPosts(1, 30);
+  //testing to see if it would fetch post
+  //console.log(result);
   return (
     <>
       <h1 className="head-text text-center">Home</h1>
