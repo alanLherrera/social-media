9dd1150a1edebe7397f4c71bc63f83984dc3ef16
alanLherrera <alanherrera06@gmail.com>
2023-10-31 13:43:35 -0700

    update to page.tsx in order to resolve break

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -2,6 +2,7 @@ import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
 
 export default async function Page({ params }: { params: { id: string } }) {
   if (!params.id) return null;
@@ -10,25 +11,25 @@ export default async function Page({ params }: { params: { id: string } }) {
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  if(!userInfo?.onboarded) redirect('/onboarding');
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const thread = await fetchThreadById(params.id);
 
-  return(
-  <section className="relative">
-    <div>
-      <ThreadCard
-        key={thread._id}
-        id={thread._id}
-        currentUserId={user?.id}
-        parentId={thread.parentIds}
-        content={thread.text}
-        author={thread.author}
-        community={thread.community}
-        createdAt={thread.createdAt}
-        comments={thread.comments}
-      />
-    </div>
-  </section>;
-  )
+  return (
+    <section className="relative">
+      <div>
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={user?.id}
+          parentId={thread.parentIds}
+          content={thread.text}
+          author={thread.author}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.comments}
+        />
+      </div>
+    </section>
+  );
 }
