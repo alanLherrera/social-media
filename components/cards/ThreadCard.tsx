7ff39a3339a07ad25c6855867b6bfb4afee530bf
alanLherrera <alanherrera06@gmail.com>
2023-11-11 13:15:23 -0800

    updates

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -37,7 +37,9 @@ const ThreadCard = ({
   isComment,
 }: Props) => {
   return (
-    <article className="flex w-full flex-col rounded-x1 bg-dark-2 p-7">
+    <article
+      className={`flex w-full flex-col rounded-x1 ${isComment}: bg-dark-2 p-7`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
@@ -93,14 +95,6 @@ const ThreadCard = ({
                   className="cursor-pointer object-container"
                 />
               </div>
-
-              {isComment && comments.length > 0 && (
-                <Link href={`/thread/${id}`}>
-                  <p className="mt-1 text-subtle-medium text-gray-1">
-                    {comments.length} Replies
-                  </p>
-                </Link>
-              )}
             </div>
           </div>
         </div>
