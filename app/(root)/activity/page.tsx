799115007c5e9dd09df2cceb95b303b474812d32
alanLherrera <alanherrera06@gmail.com>
2023-11-15 10:38:02 -0800

    user updates
    i just woke up i dont remember what changes i made honestly

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,18 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  return (
+    <section>
+      <h1 className="head-text mb-10">Activity</h1>
+    </section>
+  );
+};
+
+export default Page;
